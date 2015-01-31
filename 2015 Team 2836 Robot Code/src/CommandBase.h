f2feb4664fc26b4f39d838f38b752b9476a186b9
Luke Garceau <ltgthebeast@gmail.com>
2015-01-31 16:42:43 -0500

    Revert "Revert "4:38 1-31-15 Luke""
    This reverts commit a95fd5dec5a081dc0ca67e945a8282cbc6d4f759.

--- 2015 Team 2836 Robot Code/src/CommandBase.h
+++ 2015 Team 2836 Robot Code/src/CommandBase.h
@@ -4,6 +4,7 @@
 #include <string>
 #include "Commands/Command.h"
 #include "Subsystems/ExampleSubsystem.h"
+
 #include "OI.h"
 #include "WPILib.h"
 
@@ -21,6 +22,7 @@ public:
 	// Create a single static instance of all of your subsystems
 	static ExampleSubsystem *examplesubsystem;
 	static OI *oi;
+
 };
 
 #endif
